272dea0ecf12bb213456d20ef22061fff992088a
Andrey180293 <Lapuh767@gmail.com>
2021-12-08 09:33:15 +0300

    Finish base table logic

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,28 +1,21 @@
 import { $ } from "@core/dom";
+import { Emitter } from "@core/Emitter";
 
 export class Excel {
   constructor(selector, options) {
     this.$el = $(selector);
     this.components = options.components || [];
+    this.emitter = new Emitter();
   }
 
   getRoot() {
-    /*
-        const $root=document.createElement('div')
-        $root.classList.add('excel')
-            this.components.forEach(Component=>{
-                const $el=document.createElement('div')
-                $el.classList.add(Component.className)
-                const component = new Component($el)
-                $el.innerHTML=component.toHTML()
-               $root.append($el)
-            })
-        return $root
-*/
     const $root = $.create("div", "excel");
+    const componentOptions = {
+      emitter: this.emitter,
+    };
     this.components = this.components.map((Component) => {
       const $el = $.create("div", Component.className);
-      const component = new Component($el);
+      const component = new Component($el, componentOptions);
       $el.html(component.toHTML());
       $root.append($el);
       return component;
@@ -32,5 +25,8 @@ export class Excel {
   render() {
     this.$el.append(this.getRoot());
     this.components.forEach((component) => component.init());
+  }
+  destroy() {
+    this.components.forEach((component) => component.destroy());
   }
 }
--- src/core/ExcelComponet.js
+++ src/core/ExcelComponet.js
@@ -4,16 +4,34 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners);
     this.name = options.name;
+    this.prepare();
+    this.emitter = options.emitter;
+    this.unsubscribers = [];
   }
+  //Настроюємо наш компонент до init
+  prepare() {}
+  //Повертаэмо шаблон компонента
   toHTML() {
     return "";
   }
-
+  //Ініціалізуємо компонент
+  //додаємо слухачів
   init() {
     this.initDOMListeners();
   }
-
+  //yvedomlajem slyshatelej pro sobytije event
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args);
+  }
+  //Podpysyjemsja na podijy event
+  $on(event, fn) {
+    const unsab = this.emitter.subscribe(event, fn);
+    this.unsubscribers.push(unsab);
+  }
+  //Видаляємо компонент
+  //Чистимо слухачів
   destroy() {
     this.removeDOMListeners();
+    this.unsubscribers.forEach((unsab) => unsab());
   }
 }
